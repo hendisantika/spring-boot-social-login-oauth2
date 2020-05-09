9f0eb86cf64b64707b1ffbb6a0ff07ef72c9b849
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-09 18:46:24 +0700

    #20 - Add UserConnectionDAO

---
+++ src/main/java/com/hendisantika/springbootsocialloginoauth2/repository/UserConnectionDAO.java
@@ -0,0 +1,25 @@
+package com.hendisantika.springbootsocialloginoauth2.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-social-login-oauth2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/05/20
+ * Time: 18.46
+ */
+@Repository
+@Transactional
+public class UserConnectionDAO {
+
+    @Autowired
+    private EntityManager entityManager;
+
+}
