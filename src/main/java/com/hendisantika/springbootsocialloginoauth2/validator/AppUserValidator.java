161a03947691539d776b4a60f6b53cdfa41b7e1f
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-09 18:53:37 +0700

    #33 - Add validate in AppUserValidator

--- src/main/java/com/hendisantika/springbootsocialloginoauth2/validator/AppUserValidator.java
+++ src/main/java/com/hendisantika/springbootsocialloginoauth2/validator/AppUserValidator.java
@@ -1,9 +1,13 @@
 package com.hendisantika.springbootsocialloginoauth2.validator;
 
+import com.hendisantika.springbootsocialloginoauth2.entity.AppUser;
+import com.hendisantika.springbootsocialloginoauth2.form.AppUserForm;
 import com.hendisantika.springbootsocialloginoauth2.repository.AppUserDAO;
 import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 /**
@@ -22,5 +26,54 @@ public class AppUserValidator implements Validator {
 
     @Autowired
     private AppUserDAO appUserDAO;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz == AppUserForm.class;
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+
+        AppUserForm form = (AppUserForm) target;
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "", "Email is required");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userName", "", "User name is required");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "", "First name is required");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "", "Last name is required");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "", "Password is required");
+
+        if (errors.hasErrors()) {
+            return;
+        }
+
+        if (!emailValidator.isValid(form.getEmail())) {
+
+            errors.rejectValue("email", "", "Email is not valid");
+            return;
+        }
+
+        AppUser userAccount = appUserDAO.findAppUserByUserName(form.getUserName());
+        if (userAccount != null) {
+            if (form.getUserId() == null) {
+                errors.rejectValue("userName", "", "User name is not available");
+                return;
+            } else if (!form.getUserId().equals(userAccount.getUserId())) {
+                errors.rejectValue("userName", "", "User name is not available");
+                return;
+            }
+        }
+
+        userAccount = appUserDAO.findByEmail(form.getEmail());
+        if (userAccount != null) {
+            if (form.getUserId() == null) {
+                errors.rejectValue("email", "", "Email is not available");
+                return;
+            } else if (!form.getUserId().equals(userAccount.getUserId())) {
+                errors.rejectValue("email", "", "Email is not available");
+                return;
+            }
+        }
+    }
 
 }
