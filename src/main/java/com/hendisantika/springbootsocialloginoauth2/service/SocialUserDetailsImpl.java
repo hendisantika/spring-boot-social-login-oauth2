b6944b780d8ea0eef908d82c790875830145187e
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-09 18:50:13 +0700

    #26 - Add SocialUserDetailsImpl

---
+++ src/main/java/com/hendisantika/springbootsocialloginoauth2/service/SocialUserDetailsImpl.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springbootsocialloginoauth2.service;
+
+import com.hendisantika.springbootsocialloginoauth2.entity.AppUser;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.social.security.SocialUserDetails;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-social-login-oauth2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/05/20
+ * Time: 18.49
+ */
+public class SocialUserDetailsImpl implements SocialUserDetails {
+
+    private static final long serialVersionUID = 1L;
+    private final List<GrantedAuthority> list = new ArrayList<GrantedAuthority>();
+    private AppUser appUser;
+}
