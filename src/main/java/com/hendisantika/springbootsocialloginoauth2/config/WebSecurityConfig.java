68e9c3b6671b993b77cad27ceff178ed899b40d7
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-09 18:56:03 +0700

    #37 - Add WebSecurityConfig

---
+++ src/main/java/com/hendisantika/springbootsocialloginoauth2/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.hendisantika.springbootsocialloginoauth2.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-social-login-oauth2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/05/20
+ * Time: 18.55
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+}
