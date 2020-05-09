428b92eea22d86a74d7628b1dcd1cbab9b9e82ec
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-09 18:52:23 +0700

    #31 - Add loadUserByUsername in UserDetailsServiceImpl

--- src/main/java/com/hendisantika/springbootsocialloginoauth2/service/UserDetailsServiceImpl.java
+++ src/main/java/com/hendisantika/springbootsocialloginoauth2/service/UserDetailsServiceImpl.java
@@ -1,12 +1,19 @@
 package com.hendisantika.springbootsocialloginoauth2.service;
 
+import com.hendisantika.springbootsocialloginoauth2.entity.AppUser;
 import com.hendisantika.springbootsocialloginoauth2.repository.AppRoleDAO;
 import com.hendisantika.springbootsocialloginoauth2.repository.AppUserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,4 +33,31 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private AppRoleDAO appRoleDAO;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        System.out.println("UserDetailsServiceImpl.loadUserByUsername=" + userName);
+        AppUser appUser = this.appUserDAO.findAppUserByUserName(userName);
+
+        if (appUser == null) {
+            System.out.println("User not found! " + userName);
+            throw new UsernameNotFoundException("User " + userName + " was not found in the database");
+        }
+
+        System.out.println("Found User: " + appUser);
+
+        List<String> roleNames = this.appRoleDAO.getRoleNames(appUser.getUserId());
+
+        List<GrantedAuthority> grantList = new ArrayList<GrantedAuthority>();
+        if (roleNames != null) {
+            for (String role : roleNames) {
+                GrantedAuthority authority = new SimpleGrantedAuthority(role);
+                grantList.add(authority);
+            }
+        }
+
+        SocialUserDetailsImpl userDetails = new SocialUserDetailsImpl(appUser, roleNames);
+
+        return userDetails;
+    }
 }
--- src/main/java/com/hendisantika/springbootsocialloginoauth2/util/SecurityUtil.java
+++ src/main/java/com/hendisantika/springbootsocialloginoauth2/util/SecurityUtil.java
@@ -1,6 +1,7 @@
 package com.hendisantika.springbootsocialloginoauth2.util;
 
 import com.hendisantika.springbootsocialloginoauth2.entity.AppUser;
+import com.hendisantika.springbootsocialloginoauth2.service.SocialUserDetailsImpl;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
