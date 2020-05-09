82ce25e4325b2df4ea260b6f739f0019bb5a7e09
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-09 18:55:37 +0700

    #36 - Add getUsersConnectionRepository in SocialConfig

--- src/main/java/com/hendisantika/springbootsocialloginoauth2/config/SocialConfig.java
+++ src/main/java/com/hendisantika/springbootsocialloginoauth2/config/SocialConfig.java
@@ -1,13 +1,28 @@
 package com.hendisantika.springbootsocialloginoauth2.config;
 
 import com.hendisantika.springbootsocialloginoauth2.repository.AppUserDAO;
+import com.hendisantika.springbootsocialloginoauth2.service.ConnectionSignUpImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.security.crypto.encrypt.Encryptors;
+import org.springframework.social.UserIdSource;
 import org.springframework.social.config.annotation.ConnectionFactoryConfigurer;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurer;
+import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.connect.ConnectionSignUp;
+import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
+import org.springframework.social.connect.web.ConnectController;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+import org.springframework.social.google.connect.GoogleConnectionFactory;
+import org.springframework.social.linkedin.connect.LinkedInConnectionFactory;
+import org.springframework.social.security.AuthenticationNameUserIdSource;
+import org.springframework.social.twitter.connect.TwitterConnectionFactory;
 
 import javax.sql.DataSource;
 
@@ -58,11 +73,30 @@ public class SocialConfig implements SocialConfigurer {
                 env.getProperty("twitter.consumer.secret"));
         cfConfig.addConnectionFactory(tfactory);
 
-
     }
 
     @Override
     public UserIdSource getUserIdSource() {
         return new AuthenticationNameUserIdSource();
+    }
+
+    @Override
+    public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
+        JdbcUsersConnectionRepository usersConnectionRepository = new JdbcUsersConnectionRepository(dataSource,
+                connectionFactoryLocator, Encryptors.noOpText());
+
+        if (autoSignUp) {
+            ConnectionSignUp connectionSignUp = new ConnectionSignUpImpl(appUserDAO);
+            usersConnectionRepository.setConnectionSignUp(connectionSignUp);
+        } else {
+            usersConnectionRepository.setConnectionSignUp(null);
+        }
+        return usersConnectionRepository;
+    }
+
+    @Bean
+    public ConnectController connectController(ConnectionFactoryLocator connectionFactoryLocator,
+                                               ConnectionRepository connectionRepository) {
+        return new ConnectController(connectionFactoryLocator, connectionRepository);
     }
 }
