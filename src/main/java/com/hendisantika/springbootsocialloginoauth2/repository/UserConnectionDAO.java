b221c72735ecd9fa5cba24e43c15d9bfd7ad6cec
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-09 18:46:55 +0700

    #21 - Add findUserConnectionByUserProviderId service in UserConnectionDAO

--- src/main/java/com/hendisantika/springbootsocialloginoauth2/repository/UserConnectionDAO.java
+++ src/main/java/com/hendisantika/springbootsocialloginoauth2/repository/UserConnectionDAO.java
@@ -1,10 +1,14 @@
 package com.hendisantika.springbootsocialloginoauth2.repository;
 
+import com.hendisantika.springbootsocialloginoauth2.entity.UserConnection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +25,22 @@ public class UserConnectionDAO {
 
     @Autowired
     private EntityManager entityManager;
+
+    public UserConnection findUserConnectionByUserProviderId(String userProviderId) {
+        try {
+            String sql = "Select e from " + UserConnection.class.getName() + " e " //
+                    + " Where e.userProviderId = :userProviderId ";
+
+            Query query = entityManager.createQuery(sql, UserConnection.class);
+            query.setParameter("userProviderId", userProviderId);
+
+            @SuppressWarnings("unchecked")
+            List<UserConnection> list = query.getResultList();
+
+            return list.isEmpty() ? null : list.get(0);
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
 
 }
