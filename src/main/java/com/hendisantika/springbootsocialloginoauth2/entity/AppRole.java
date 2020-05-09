e4c0626f4263168d0954d535039bc3599657874c
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-09 18:06:51 +0700

    #4 - Add AppRole Entity

---
+++ src/main/java/com/hendisantika/springbootsocialloginoauth2/entity/AppRole.java
@@ -0,0 +1,49 @@
+package com.hendisantika.springbootsocialloginoauth2.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-social-login-oauth2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/05/20
+ * Time: 18.04
+ */
+@Entity
+@Table(name = "App_Role", uniqueConstraints = {@UniqueConstraint(name = "APP_ROLE_UK", columnNames = "Role_Name")})
+public class AppRole {
+
+    public static final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_ADMIN = "ROLE_ADMIN";
+
+    @Id
+    @GeneratedValue
+    @Column(name = "Role_Id", nullable = false)
+    private Long roleId;
+
+    @Column(name = "Role_Name", length = 30, nullable = false)
+    private String roleName;
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
