2a9d4b33c461ca6380f7a28e1f3c87b4a81c9a08
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-09 18:43:21 +0700

    #12 - Add createRoleFor service in AppRoleDAO

--- src/main/java/com/hendisantika/springbootsocialloginoauth2/repository/AppRoleDAO.java
+++ src/main/java/com/hendisantika/springbootsocialloginoauth2/repository/AppRoleDAO.java
@@ -1,6 +1,7 @@
 package com.hendisantika.springbootsocialloginoauth2.repository;
 
 import com.hendisantika.springbootsocialloginoauth2.entity.AppRole;
+import com.hendisantika.springbootsocialloginoauth2.entity.AppUser;
 import com.hendisantika.springbootsocialloginoauth2.entity.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -46,6 +47,24 @@ public class AppRoleDAO {
             return (AppRole) query.getSingleResult();
         } catch (NoResultException e) {
             return null;
+        }
+    }
+
+    public void createRoleFor(AppUser appUser, List<String> roleNames) {
+        //
+        for (String roleName : roleNames) {
+            AppRole role = this.findAppRoleByName(roleName);
+            if (role == null) {
+                role = new AppRole();
+                role.setRoleName(AppRole.ROLE_USER);
+                this.entityManager.persist(role);
+                this.entityManager.flush();
+            }
+            UserRole userRole = new UserRole();
+            userRole.setAppRole(role);
+            userRole.setAppUser(appUser);
+            this.entityManager.persist(userRole);
+            this.entityManager.flush();
         }
     }
 }
