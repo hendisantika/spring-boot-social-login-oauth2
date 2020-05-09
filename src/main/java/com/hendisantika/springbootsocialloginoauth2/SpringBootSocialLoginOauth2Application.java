1c514011f5469052c37dd524bf869ad1b30d0b92
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-09 18:01:29 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootsocialloginoauth2/SpringBootSocialLoginOauth2Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootsocialloginoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSocialLoginOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSocialLoginOauth2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootsocialloginoauth2/SpringBootSocialLoginOauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootsocialloginoauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSocialLoginOauth2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
