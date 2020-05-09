56c0adea3f1e00786d91128e07dab2ea60d98e11
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-10 03:57:35 +0700

    #50 - Add signupSave in MainController

--- src/main/java/com/hendisantika/springbootsocialloginoauth2/controller/MainController.java
+++ src/main/java/com/hendisantika/springbootsocialloginoauth2/controller/MainController.java
@@ -1,22 +1,35 @@
 package com.hendisantika.springbootsocialloginoauth2.controller;
 
+import com.hendisantika.springbootsocialloginoauth2.entity.AppRole;
+import com.hendisantika.springbootsocialloginoauth2.entity.AppUser;
 import com.hendisantika.springbootsocialloginoauth2.form.AppUserForm;
 import com.hendisantika.springbootsocialloginoauth2.repository.AppUserDAO;
+import com.hendisantika.springbootsocialloginoauth2.util.SecurityUtil;
 import com.hendisantika.springbootsocialloginoauth2.util.WebUtils;
 import com.hendisantika.springbootsocialloginoauth2.validator.AppUserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.UsersConnectionRepository;
+import org.springframework.social.connect.web.ProviderSignInUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -125,5 +138,41 @@ public class MainController {
         }
         model.addAttribute("myForm", myForm);
         return "signupPage";
+    }
+
+    @PostMapping(value = {"/signup"})
+    public String signupSave(WebRequest request, //
+                             Model model, //
+                             @ModelAttribute("myForm") @Validated AppUserForm appUserForm, //
+                             BindingResult result, //
+                             final RedirectAttributes redirectAttributes) {
+
+        // Validation error.
+        if (result.hasErrors()) {
+            return "signupPage";
+        }
+
+        List<String> roleNames = new ArrayList<String>();
+        roleNames.add(AppRole.ROLE_USER);
+
+        AppUser registered = null;
+
+        try {
+            registered = appUserDAO.registerNewUserAccount(appUserForm, roleNames);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            model.addAttribute("errorMessage", "Error " + ex.getMessage());
+            return "signupPage";
+        }
+
+        if (appUserForm.getSignInProvider() != null) {
+            ProviderSignInUtils providerSignInUtils //
+                    = new ProviderSignInUtils(connectionFactoryLocator, connectionRepository);
+            providerSignInUtils.doPostSignUp(registered.getUserName(), request);
+        }
+
+        SecurityUtil.logInUser(registered, roleNames);
+
+        return "redirect:/userInfo";
     }
 }
