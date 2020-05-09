53eda55e552ac3eb16b99c736d94283db511ad50
Hendi Santika <hendisantika@yahoo.co.id>
2020-05-09 18:42:49 +0700

    #10 - Add getRoleNames service in AppRoleDAO

--- src/main/java/com/hendisantika/springbootsocialloginoauth2/repository/AppRoleDAO.java
+++ src/main/java/com/hendisantika/springbootsocialloginoauth2/repository/AppRoleDAO.java
@@ -1,10 +1,13 @@
 package com.hendisantika.springbootsocialloginoauth2.repository;
 
+import com.hendisantika.springbootsocialloginoauth2.entity.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +24,13 @@ public class AppRoleDAO {
 
     @Autowired
     private EntityManager entityManager;
+
+    @SuppressWarnings("unchecked")
+    public List<String> getRoleNames(Long userId) {
+        String sql = "Select ur.appRole.roleName from " + UserRole.class.getName() + " ur "
+                + " where ur.appUser.userId = :userId ";
+        Query query = this.entityManager.createQuery(sql, String.class);
+        query.setParameter("userId", userId);
+        return query.getResultList();
+    }
 }
